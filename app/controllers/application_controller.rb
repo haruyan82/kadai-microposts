d4d78a9ee9edca611dca8035636ee6e1d1d7e8e3
yohiharu.yanagi <haruyan82@gmail.com>
2019-08-27 11:08:13 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,14 @@ class ApplicationController < ActionController::Base
     
     private
     
+    
     def require_user_logged_in
       unless logged_in?
-        redirect_to root_url
+        redirect_to login_url
       end
+    end
+    
+    def counts(user)
+      @count_microposts = user.microposts.count
     end
 end
