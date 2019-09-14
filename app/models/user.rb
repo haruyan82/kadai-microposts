ff76a6ba2b5cf0f725d13f507ec650ba72759bd0
yohiharu.yanagi <haruyan82@gmail.com>
2019-09-14 04:47:37 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,12 @@ class User < ApplicationRecord
     
     has_many :microposts
     has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverses_of_relationship, source: :user
+    has_many :followings, through: :relationships, source: :follow
+    has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+    has_many :followers, through: :reverses_of_relationship, source: :user
+    
+    has_many :favorites
+    has_many :fav_posts, through: :favorites,source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -29,6 +32,19 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def like(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def fav_post?(micropost)
+    self.fav_posts.include?(micropost)
   end
 end
 
